84a4a58f33b8d8c570f71405a5f946ac002517a5
Shimran George & David Zulaica <student@epicodus.com>
2015-01-28 12:27:27 -0800

    Add normalize_text callback method.

--- lib/survey.rb
+++ lib/survey.rb
@@ -1,3 +1,11 @@
 class Survey < ActiveRecord::Base
   has_many(:questions)
+  before_save(:normalize_title)
+
+private
+
+  define_method(:normalize_title) do
+    self.title = (title().capitalize())
+  end
+
 end
